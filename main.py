43d134b9731bb747ae95870c1c2cb355b7b846da
Rob Lawyer <lroblawyer@gmail.com>
2024-04-15 20:50:39 -0400

    Added letter count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     book = read_book("books/frankenstein.txt")
-    print(count_words(book))
+    print(count_letters(book))
 
 
 def read_book(path):
@@ -12,6 +12,16 @@ def read_book(path):
 def count_words(book):
     words = book.split()
     return len(words)
+
+
+def count_letters(book):
+    letters = {chr(ord("a") + i): 0 for i in range(0, 26)}
+
+    for character in book:
+        character = character.lower()
+        if character in letters:
+            letters[character] += 1
+    return letters
 
 
 main()
